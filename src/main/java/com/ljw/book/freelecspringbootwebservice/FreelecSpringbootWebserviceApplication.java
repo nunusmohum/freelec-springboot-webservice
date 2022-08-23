5d5303681c29ae53241df02b62e3f2bd4dea65f6
nunusmohum <halojw301@gmail.com>
2022-08-23 10:33:34 +0900

    Initial commit

---
+++ src/main/java/com/ljw/book/freelecspringbootwebservice/FreelecSpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ljw.book.freelecspringbootwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringbootWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelecSpringbootWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ljw/book/freelecspringbootwebservice/FreelecSpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljw.book.freelecspringbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringbootWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
